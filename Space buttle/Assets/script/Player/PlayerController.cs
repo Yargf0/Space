e6233b23fd2733777aa99f8978095907d2711e01
Yargf0 <122349983+Yargf0@users.noreply.github.com>
2023-12-12 20:01:30 +0300

    ++

--- Space buttle/Assets/script/Player/PlayerController.cs
+++ Space buttle/Assets/script/Player/PlayerController.cs
@@ -4,28 +4,51 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : ShipBasic
-{
+{ 
     public static PlayerController Instance { get; private set; }
     private Rigidbody2D rb2D;
     [SerializeField] private List<ParticleSystem> engineParticle;
+    [SerializeField] private float axeleration;
+    [SerializeField] private Image staminaSprite;
+    [Range(0,100)][SerializeField] private float stamina;
+    private float maxStamina;
+
     public override void Awake()
     {
         Hp = Ship.Hp;
         maxHp = Hp;
 
+        maxStamina = stamina;
+
         Instance =this;
         Player = true;
         rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
+
     public void FixedUpdate()
     {
-        if(Input.GetKey("w"))
+        float ax = 1;
+        stamina += 0.1f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (stamina > 1)
+            {
+                ax = axeleration;
+                stamina -= 0.3f;
+            }
+        }
+        if (staminaSprite != null)
+        {
+            staminaSprite.fillAmount = (stamina / maxStamina);
+        }
+
+        if (Input.GetKey("w"))
         {                
-            rb2D.AddForce(gameObject.transform.up * Ship.Speed * Time.deltaTime, ForceMode2D.Force);
+            rb2D.AddForce(gameObject.transform.up * Ship.Speed * ax * Time.deltaTime, ForceMode2D.Force);
         }
         if (Input.GetKey("s"))
         {
-            rb2D.AddForce(-gameObject.transform.up * Ship.Speed * Time.deltaTime, ForceMode2D.Force);
+            rb2D.AddForce(-gameObject.transform.up * Ship.Speed * ax * 0.6f * Time.deltaTime, ForceMode2D.Force);
         }
         if (Input.GetKey("a"))  
         {
